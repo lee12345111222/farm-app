ad7fcd14aeca2c8620618d49f1a6849df31a5e88
lee12345111222 <177575822503@163.com>
2024-04-01 22:40:11 +0800

    add:console

--- next.config.js
+++ next.config.js
@@ -38,9 +38,9 @@ const nextConfig = withTM(
     },
     // reactStrictMode: true, // Enable React strict mode for improved error handling
     // swcMinify: true, // Enable SWC minification for improved performance
-    // compiler: {
-    //   removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
-    // },
+    compiler: {
+      removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+    },
   })
 );
 
